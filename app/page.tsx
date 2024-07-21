541168b8ed7acbd36d5ef832e4dda1cf0d9f41ee
Govindapalle Vijaychandra <118375565+Vijaychandra-Govindapalle@users.noreply.github.com>
2024-07-21 19:24:36 +0530

    Search for songs

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import { getSongs } from "./lib/data";
 import ScrollScreen from "./ui/scrollScreen";
+import GetSongs from "./ui/searchSongs/page";
+import { getSong } from './lib/data';
 
 export default async function Home() {
-  const songs = await getSongs();
-  //console.log(songs)
+  const songs = await getSong('Faded');
+  const dummySongs = await getSongs()
+  console.log(songs)
   return (
-    <ScrollScreen songs={songs} />
+    <>
+    <ScrollScreen songs={dummySongs} /> </>
+   
+  
+   // 
+   
   );
 }
  
--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,28 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'loremflickr.com',
                // pathname: '/640/480/nature'
+            },
+            {
+                protocol: 'https',
+                hostname:'i.scdn.co',
             }
           ],
-    }
+    },
+    async headers() {
+      return [
+          {
+              // matching all API routes
+              source: "/api/:path*",
+              headers: [
+                  { key: "Access-Control-Allow-Credentials", value: "true" },
+                  { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, // replace this your actual origin
+                  { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, Cache-Control ,X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+              ]
+          }
+      ]
+  }
+    
 };
 
 export default nextConfig;
